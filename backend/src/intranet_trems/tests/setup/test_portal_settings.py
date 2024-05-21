ea30a442a6045b6a02dc44531e10d54ae5b82409
Érico Andrei <ericof@gmail.com>
2024-05-21 16:46:56 -0400

    Exercício 13: Testa outras configurações do portal

--- backend/src/intranet_trems/tests/setup/test_portal_settings.py
+++ backend/src/intranet_trems/tests/setup/test_portal_settings.py
@@ -1,12 +1,24 @@
-"""Portal settings tests."""
 from plone import api
 
 
-class TestPortalSettings:
-    """Test that Portal configuration is correctly done."""
+class TestPortalProperties:
 
     def test_portal_title(self, portal):
-        """Test portal title."""
-        value = api.portal.get_registry_record("plone.site_title")
         expected = "Nova Intranet TRE-MS"
-        assert value == expected
+        value = api.portal.get_registry_record("plone.site_title")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
+
+    def test_portal_timezone(self, portal):
+        expected = "America/Campo_Grande"
+        value = api.portal.get_registry_record("plone.portal_timezone")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
+
+    def test_portal_sitemap(self, portal):
+        expected = True
+        value = api.portal.get_registry_record("plone.enable_sitemap")
+        assert value is expected, f"Value '{value}' is not '{expected}'"
+
+    def test_portal_email_from_name(self, portal):
+        expected = "Nova Intranet TRE-MS"
+        value = api.portal.get_registry_record("plone.email_from_name")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
